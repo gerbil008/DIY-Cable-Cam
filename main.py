260f84095451d895ffeb712b5c646af56d91b2e9
gerbil008 <163928546+gerbil008@users.noreply.github.com>
2024-07-08 17:58:09 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,15 +3,23 @@ import usocket as socket
 import random
 import struct
 import time
+import utime
 from machine import UART, Pin, PWM
 from micropython import const
 import gc
 import _thread
+import machine
+from machine import RTC
+rtc = RTC()
+with open("hallo", "w+") as f:
+    f.write(str(rtc.datetime()))
+    f.close()   
 gc.collect()
 print(gc.mem_free())
-ssid = "Among_Sus"
+active = True
+ssid = "Crash-Cam"
 password1 = "passwort123"
-file = open("index1.html", "r")
+file = open("index.html", "r")
 html = file.read()
 file.close()
 i1 = PWM(Pin(19))
@@ -26,12 +34,25 @@ full1 = 65069
 led1 = Pin(23, Pin.OUT)
 led2 = Pin(32, Pin.OUT)
 led3 = Pin(22, Pin.OUT)
-domains = ["cable.cam", "apple.shit", "windows.noob"]
-
-
+domains = ["cable.cam", "apple.shit", "windows.noob", "linux.nice", "cable.cum"]
+def errlog(msg):
+    file = open("log.log", "a")
+    file.write(msg+"\n")
+    file.close()
+def delete_first_n_lines(filename, n):
+        with open(filename, 'r') as file:
+            lines = file.readlines()
+        if len(lines) >= 13000:
+            errlog(f"Logfile has under 13000 lines ({len(lines)})")
+        else:
+            lines = lines[n:]
+            errlog(f"Logfile has over 13000 lines ({len(lines)})")
+        with open(filename, 'w') as file:
+            file.writelines(lines)            
+errlog("Started")
+#delete_first_n_lines("log.log", 5000)
 def ip_to_bytes(ip):
     return bytes(map(int, ip.split('.')))
-
 def dns_response(data, ip):
     transaction_id = data[:2]
     flags = b'\x81\x80'
@@ -48,7 +69,6 @@ def dns_response(data, ip):
 def start_dns_server():
     dns_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     dns_socket.bind(('', 53))
-
     while True:
         try:
             data, addr = dns_socket.recvfrom(512)
@@ -74,12 +94,15 @@ def start_dns_server():
             domain = domain.decode('utf-8')
             if domain in domains:
                 dns_socket.sendto(dns_response(data, ap.ifconfig()[0] ), addr)
-        except:
+        except Exception as e:
             print("DNS Error")
+            errlog(f"DNS-Error {e}")
+            run_motor(0)
+            led3.on()
 
 def run_motor(pwm_num):
     led2.on()
-    print(round(abs(full1*pwm_num*0.01)))
+    errlog(f"Run motor with {pwm_num}")
     print(pwm_num)
     if pwm_num < 0:
         i1.duty_u16(round(abs(full1*pwm_num*0.01)))
@@ -97,7 +120,19 @@ def make_hotspot():
     while ap.active() == False:
         pass
     print(ap.ifconfig()[0])
+def timer1():
+    #print("Started")
+    global active
+    active = True
+    for i in range(13):
+        if active:
+            time.sleep(0.1)
+        else:
+            break
+    if active:
+        run_motor(0)
 def start_server():
+    global active
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.bind(('', 80))
     s.listen(1)
@@ -109,6 +144,11 @@ def start_server():
             conn.sendall('Content-Type: text/html\n')
             conn.sendall('Connection: close\n\n')
             conn.sendall(html)
+            #if b"ping" in request:
+                #print("Got")
+                #active = False
+                #time.sleep(0.1)
+                #_thread.start_new_thread(timer1, ())
             if b'value=' in request:
                 index = request.find(b'value=')
                 wert = request[index+6:index+9].decode()
@@ -125,17 +165,25 @@ def start_server():
                 try:        
                     run_motor(int(h))
                     led3.off()
-                except:
+                except Exception as e:
                     led3.on()
+                    run_motor(0)
+                    errlog(f"Motor-Error {e}")
         conn.close()
 def main():
-    make_hotspot()
+    try:
+        make_hotspot()
+    except Exception as e:
+        errlog(f"Hotspot-Error {e}")
+        led3.on()
+        run_motor(0)
     _thread.start_new_thread(start_dns_server, ())
-    start_server()
+    try:
+        start_server()
+    except Exception as e:
+        errlog(f"Server-Error {e}")
+        run_motor(0)
+        led3.on()
 
 if __name__ == "__main__":
     main()
-
-
-
-
